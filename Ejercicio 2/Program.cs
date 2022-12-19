7ec26768803aa5dc8b94afa0b87cd22dcaf67af5
MartoJuanDev <104855025+Marto85@users.noreply.github.com>
2022-12-19 18:03:21 -0300

    Subido el ejercicio 2

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string time = DateTime.Now.ToString("h:mm:ss tt");
+            Console.WriteLine("La hora actual es {0}", time);
+        }
+    }
+}
