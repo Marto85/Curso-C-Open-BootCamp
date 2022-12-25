e16b0336c04b5031bbda0886ce7d0e65fdd1ec77
MartoJuanDev <104855025+Marto85@users.noreply.github.com>
2022-12-24 21:04:03 -0300

    Subo Ejercicio 1 Sesion 5

---
+++ Ejercicio 10/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ejercicio_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejercicio 1 de la sesion 5
+            Console.WriteLine("Buenos dias. Vamos a solicitarles ciertos datos");
+            Console.WriteLine("Por favor, indique su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Por favor, indique su email");
+            string email = Console.ReadLine();
+            Console.WriteLine("Por favor, indique si dispone de un cupon de descuento. Indique si o no");
+            bool cupon = Convert.ToBoolean(Console.ReadLine());
+
+            if (cupon == true)
+            {
+                Console.WriteLine("Por favor, indique el valor del producto");
+                double valorProducto = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Por favor, indique el % de descuento que dispone");
+                double cuponDesc = Convert.ToDouble(Console.ReadLine());
+                double pctCupon = cuponDesc / 100;
+                Console.WriteLine("El valor final de la compra es de " + (valorProducto - (valorProducto * pctCupon)));
+            }
+            else
+            {
+                Console.WriteLine("Por favor, indique el valor de la compra");
+                double valorCompra = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("El valor final de la compra es de " + valorCompra);
+            }
+        }
+    }
+}
---
+++ Ejercicio 11/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ejercicio_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejercicio 2 de la sesion 5
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
